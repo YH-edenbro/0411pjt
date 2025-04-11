8c0bd8908d0f0c1f2230689eaf88a121298969a4
Y00CHAN <kaiwow@naver.com>
2025-04-11 14:34:50 +0900

    Yuchan

--- movies/forms.py
+++ movies/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from .models import Movie, Comment
 
-
-# form1 (일반 Form 예시)
 class CreateForm(forms.ModelForm):
 
     class Meta:
@@ -17,70 +15,3 @@ class CreateForm(forms.ModelForm):
         }
 
 
-# form2 (ModelForm 예시)
-class ProductForm(forms.ModelForm):
-    CATEGORY_CHOICES = [
-        ('ELEC', 'Electronics'),
-        ('BOOK', 'Books'),
-        ('FASH', 'Fashion'),
-    ]
-    category = forms.MultipleChoiceField(
-        choices=CATEGORY_CHOICES,
-        required=False,
-        help_text='하나 이상의 카테고리를 선택하세요',
-        widget=forms.CheckboxSelectMultiple,  # 체크박스 형태로 랜더링
-    )
-
-    class Meta:
-        model = Movie
-        fields = '__all__'
-
-# # form2 (ModelForm 예시)
-# class ProductForm(forms.ModelForm):
-#     CATEGORY_CHOICES = [
-#         ('ELEC', 'Electronics'),
-#         ('BOOK', 'Books'),
-#         ('FASH', 'Fashion'),
-#     ]
-#     category = forms.MultipleChoiceField(
-#         choices=CATEGORY_CHOICES,
-#         required=False,
-#         help_text='하나 이상의 카테고리를 선택하세요',
-#         widget=forms.CheckboxSelectMultiple,  # 체크박스 형태로 랜더링
-#     )
-
-#     class Meta:
-#         model = Product
-#         fields = ['name', 'price', 'category']
-
-
-# # form3
-# class BaseForm(forms.Form):
-#     name = forms.CharField(max_length=50)
-#     email = forms.EmailField()
-
-
-# # form4
-# class ExtendedForm(BaseForm):
-#     address = forms.CharField(max_length=100)
-
-
-# # form5
-# class WidgetForm(forms.Form):
-#     username = forms.CharField(
-#         widget=forms.TextInput(
-#             attrs={
-#                 'class': 'form-control',
-#             },
-#         )
-#     )
-#     bio = forms.CharField(
-#         widget=forms.Textarea(
-#             attrs={
-#                 'rows': 5,
-#                 'cols': 50,
-#                 'style': 'resize: none;',
-#             }
-#         ),
-#         required=False,
-#     )
