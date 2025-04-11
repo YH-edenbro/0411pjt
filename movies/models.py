435cdb42dab939a5cb4daf2c4cfa58a70c9760dc
YH-edenbro <hyun96227@gmail.com>
2025-04-11 11:55:48 +0900

    models

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Movie(models.Model):
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    genre = models.CharField(max_length=50)
+    score = models.FloatField()
+
+
+class Comment(models.Model):
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
