bcb18c4dac7a60a124fbfc59098770725481b3d7
YH-edenbro <hyun96227@gmail.com>
2025-04-11 13:45:01 +0900

    movies.urls

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from . import views
 
 app_name = 'movies'
 urlpatterns = [
-    path()
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('<int:movie_pk>/', views.detail, name='detail'),
+    path('<int:movie_pk>/update/', views.update, name='update'),
+    path('<int:movie_pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/comments_create/', views.comments_create, name='comments_create'),
+    path('<int:pk>/comments_delete/', views.comments_delete, name='comments_delete'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Movie, Comment
 
 # Create your views here.
+def index(request):
+    movies = Movie.objects.all()
+    context = {
+        'movies':movies
+    }
+    return render(request, 'movies/index.html', context)
